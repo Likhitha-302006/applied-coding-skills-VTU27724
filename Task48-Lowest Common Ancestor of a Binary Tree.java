cbd6b8c03b94c9c1b6a094b2c1011bca7725f581
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-09-10 09:56:51 +0530

    Add files via upload

---
+++ Task48-Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+               if (root == null || root == p || root == q) {
+            return root;
+        }
+
+             TreeNode left = lowestCommonAncestor(root.left, p, q);
+
+                TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+               if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
+
+
+
+Input
+root =
+[3,5,1,6,2,0,8,null,null,7,4]
+p =
+5
+q =
+1
+Output
+3
+Expected
+3
